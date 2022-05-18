cd7e7067b49b2c516988f06c6a746aba1c2d783f
Kaydarin <zim50_killerz@yahoo.com>
2022-05-18 22:11:14 +0800

    Created chat UI.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/home';
+import User1 from './pages/user1';
+import User2 from './pages/user2';
+import { Button } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul className="header-menu">
+            <li className="menus">
+              <Link to="/">
+                <Button colorScheme='teal' size='lg'>Home</Button>
+              </Link>
+            </li>
+            <li className="menus">
+              <Link to="/user1">
+                <Button colorScheme='teal' size='lg'>User 1</Button>
+              </Link>
+            </li>
+            <li className="menus">
+              <Link to="/user2" target="_blank">
+                <Button colorScheme='teal' size='lg'>User 2</Button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={<Home />}>
+          </Route>
+          <Route path="/user1" element={<User1 />}>
+          </Route>
+          <Route path="/user2" element={<User2 />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/user1/index.js
@@ -0,0 +1,66 @@
+import { Stack, Input, IconButton, Tag, TagLabel } from "@chakra-ui/react"
+import { FaPaperPlane } from "react-icons/fa";
+import './style.css'
+export default function User1() {
+    return (
+        <div>
+            <div className="chat-container">
+                <Stack spacing={3}>
+                    <div>
+                        <div className="text-box-response">
+                            <p>Hi! My name is Jogaila</p>
+                            <div className="text-box-tag">
+                                <Tag size='lg' key='lg' variant='subtle' colorScheme='cyan'>
+                                    <TagLabel>Other User</TagLabel>
+                                </Tag>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="text-box-response">
+                            <p>What is your name?</p>
+                            <div className="text-box-tag">
+                                <Tag size='lg' key='lg' variant='subtle' colorScheme='cyan'>
+                                    <TagLabel>Other User</TagLabel>
+                                </Tag>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="text-box-reply">
+                            <div className="text-box-tag">
+                                <Tag size='lg' key='lg' variant='subtle' colorScheme='blue'>
+                                    <TagLabel>You</TagLabel>
+                                </Tag>
+                            </div>
+                            <p>Nice to meet you Jogaila!</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="text-box-reply">
+                            <div className="text-box-tag">
+                                <Tag size='lg' key='lg' variant='subtle' colorScheme='blue'>
+                                    <TagLabel>You</TagLabel>
+                                </Tag>
+                            </div>
+                            <p>My name is Jadwiga!</p>
+                        </div>
+                    </div>
+                </Stack>
+            </div>
+            <div className="input-group">
+                <div className="input-container">
+                    <Input placeholder='Enter chat here' size='lg' className="chat-input" />
+                </div>
+                <div className="input-icon-container">
+                    <IconButton
+                        colorScheme='teal'
+                        aria-label='Send Chat'
+                        size='lg'
+                        icon={<FaPaperPlane />}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
