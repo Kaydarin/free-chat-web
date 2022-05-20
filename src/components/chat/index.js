96e41cadf0d9cfd6433d93017488eb5ba0738513
Kaydarin <zim50_killerz@yahoo.com>
2022-05-20 22:53:55 +0800

    Fix messages rendering bug.

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,47 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Stack, Input, IconButton, Tag, TagLabel } from "@chakra-ui/react"
 import { FaPaperPlane } from "react-icons/fa";
 import './style.css'
 
 export default function Chat(props) {
 
-    const [webSocket, setWebSocket] = useState(null);
+    const webSocket = useRef(new WebSocket('ws://localhost:8100/ws/connect'))
     const [messageToSend, setMessageToSend] = useState('');
     const [messages, setMessages] = useState([]);
 
 
-    useEffect(() => {
-
-        const connectWebsocket = () => {
-            const socket = new WebSocket('ws://localhost:8100/ws/connect');
-
-            // Connection opened
-            socket.addEventListener('open', function (event) {
-                console.log('Websocket connected')
-                setWebSocket(socket);
-            });
-
-            // Listen for messages
-            socket.addEventListener('message', function (event) {
-                console.log('Message from server ', event.data);
-                updateMessages(event.data);
-            });
-        }
-
-        const updateMessages = (sockMessage) => {
-            const msg = [...messages];
+    const updateMessages = (sockMessage) => {
+        setMessages(state => {
+            const msg = [...state];
             msg.push(JSON.parse(sockMessage));
-            setMessages(msg);
-        }
+            return msg
+        });
+    }
 
-        if (webSocket == null) {
-            connectWebsocket();
-        }
-    }, [webSocket, messages]);
 
     useEffect(() => {
-        console.log(messages);
-    }, [messages])
+
+        webSocket.current.onopen = () => {
+            console.log('Websocket connected')
+            webSocket.current.onmessage = (e) => {
+                console.log('Message from server ', e.data);
+                updateMessages(e.data);
+            }
+        }
+    }, []);
 
     const handleChatInput = (e) => {
         setMessageToSend(e.target.value);
@@ -50,7 +37,7 @@ export default function Chat(props) {
     const sendMessage = () => {
 
         if (messageToSend !== '') {
-            webSocket.send(JSON.stringify({
+            webSocket.current.send(JSON.stringify({
                 user: props.user,
                 message: messageToSend
             }));
@@ -65,42 +52,40 @@ export default function Chat(props) {
         }
     }
 
-    const renderMessage = (txt, index) => {
-        if (txt.user === props.user) {
-            return (
-                <div key={index}>
-                    <div className="text-box-response">
-                        <p>{txt.message}</p>
-                        <div className="text-box-tag">
-                            <Tag size='lg' key='lg' variant='subtle' colorScheme='cyan'>
-                                <TagLabel>{txt.user}</TagLabel>
-                            </Tag>
-                        </div>
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <div key={index}>
-                    <div className="text-box-reply">
-                        <div className="text-box-tag">
-                            <Tag size='lg' key='lg' variant='subtle' colorScheme='blue'>
-                                <TagLabel>{txt.user}</TagLabel>
-                            </Tag>
-                        </div>
-                        <p>{txt.message}</p>
-                    </div>
-                </div>
-            );
-        }
-    }
-
     return (
         <div>
             <div className="chat-container">
                 <Stack spacing={3}>
                     {
-                        messages && messages.map((txt, i) => renderMessage(txt, i))
+                        messages && messages.map((txt, i) => {
+                            if (txt.user === props.user) {
+                                return (
+                                    <div key={i}>
+                                        <div className="text-box-response">
+                                            <p>{txt.message}</p>
+                                            <div className="text-box-tag">
+                                                <Tag size='lg' key='lg' variant='subtle' colorScheme='cyan'>
+                                                    <TagLabel>{txt.user}</TagLabel>
+                                                </Tag>
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            } else {
+                                return (
+                                    <div key={i}>
+                                        <div className="text-box-reply">
+                                            <div className="text-box-tag">
+                                                <Tag size='lg' key='lg' variant='subtle' colorScheme='blue'>
+                                                    <TagLabel>{txt.user}</TagLabel>
+                                                </Tag>
+                                            </div>
+                                            <p>{txt.message}</p>
+                                        </div>
+                                    </div>
+                                );
+                            }
+                        })
                     }
                     {/* <div>
                         <div className="text-box-response">
